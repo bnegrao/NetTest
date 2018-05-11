c31e1179f76358805bf199e28d4f07b607c34ff5
Bruno Negrao G Zica <bnegrao@gmail.com>
2018-05-10 22:39:49 -0300

    Initial commit

---
+++ src/main/java/com/bnegrao/NetTest/NetTestApplication.java
@@ -0,0 +1,12 @@
+package com.bnegrao.NetTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetTestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bnegrao/NetTest/NetTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bnegrao.NetTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NetTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
